2c13ab4785bbfd8ed6df17a1e7739df7f9ca1eb1
magicdawn <magicdawn@qq.com>
2017-03-09 16:46:56 +0800

    eslint: rm semi

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-'use strict';
+'use strict'
 
 /**
  * main exports
  */
 
 module.exports = function promiseObj(o) {
-  var ks = Object.keys(o);
-  var result = {};
+  var ks = Object.keys(o)
+  var result = {}
 
   var ps = ks.map(function(key) {
-    var value = o[key];
+    var value = o[key]
     return Promise.resolve(value).then(function(val) {
-      result[key] = val;
-    });
-  });
+      result[key] = val
+    })
+  })
 
   return Promise.all(ps).then(function() {
-    return result;
-  });
-};
+    return result
+  })
+}
--- test/simple.js
+++ test/simple.js
@@ -1,20 +1,20 @@
-'use strict';
+'use strict'
 
 /**
  * module dependencies
  */
 
-const pobj = require('../');
-const should = require('should');
+const pobj = require('../')
+const should = require('should')
 
 describe('It works', function() {
   const fn = function(x) {
-    return Promise.resolve(x);
-  };
+    return Promise.resolve(x)
+  }
 
   const errFn = function(msg) {
-    return Promise.reject(new Error(msg));
-  };
+    return Promise.reject(new Error(msg))
+  }
 
   it('normal works', async function() {
     const o = await pobj({
@@ -23,21 +23,21 @@ describe('It works', function() {
       x: '1',
       y: 2,
       z: null
-    });
+    })
     
-    o.hello.should.equal('hello');
-    o.world.should.equal('world');
-  });
+    o.hello.should.equal('hello')
+    o.world.should.equal('world')
+  })
 
   it('error works', async function() {
     try {
       const o = await pobj({
         hello: fn('hello'),
         world: errFn('world')
-      });
+      })
     } catch (e) {
-      e.should.instanceof(Error);
-      e.message.should.equal('world');
+      e.should.instanceof(Error)
+      e.message.should.equal('world')
     }
-  });
-});
+  })
+})
